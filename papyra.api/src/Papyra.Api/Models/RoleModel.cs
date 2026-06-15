3cee184336f5fc9f252c24be065583351027b0a1
Rahul Anand <41159815+rahulnsanand@users.noreply.github.com>
2026-06-15 19:34:10 +0530

    feat: barebones app overhaul prep

--- papyra.api/src/Papyra.Api/Models/RoleModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Papyra.Api.Models;
-
-public sealed class RoleModel
-{
-    public required string Name              { get; set; }
-    public int   MaxNotesAllowed             { get; set; } = 500;
-    public bool  AllowFileUploads            { get; set; } = true;
-    public int   AttachmentSizeLimitMB       { get; set; } = 16;
-}
