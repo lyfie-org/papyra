38c23aee5d48475538a76589f2a18cc39efe6ef4
Rahul Anand <41159815+rahulnsanand@users.noreply.github.com>
2026-08-07 12:25:33 +0530

    fix: lint fix

--- papyra.web/src/main.tsx
+++ papyra.web/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/queryClient.ts';
-import { ThemeProvider } from './hooks/useTheme.tsx';
+import { ThemeProvider } from './hooks/ThemeProvider.tsx';
 import './index.css';
 import App from './App.tsx';
 
